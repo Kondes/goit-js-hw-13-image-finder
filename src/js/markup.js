02882c06d615f0e74d89fd24771d3b196f4767f6
Andrii Kondysiuk <kondysiuka@gmail.com>
2021-03-22 22:31:02 +0200

    scroll update

--- src/js/markup.js
+++ src/js/markup.js
@@ -5,24 +5,37 @@ const refs = {
   searchForm: document.querySelector('.search-form'),
   photoContainer: document.querySelector('.gallery'),
   loadMoreButton: document.querySelector('[data-action="load-more"]'),
+
 };
 
 const pictureApiService = new PictureApiService();
+
 
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreButton.addEventListener('click', onLoadMore);
 
 function onSearch(e) {
   e.preventDefault();
+  
 
   clearHitsContainer();
   pictureApiService.query = e.currentTarget.elements.query.value;
   pictureApiService.resetPage();
-  pictureApiService.fetchArticles().then(appendHitsMarkup);
+  pictureApiService
+    .fetchArticles()
+    .then(appendHitsMarkup)
+    .then(() => refs.loadMoreButton.classList.remove('disabled'));
+}
+
+function scroll() {
+  window.scrollTo({
+    top: window.pageYOffset + 484,
+    behavior: 'smooth',
+  });
 }
 
 function onLoadMore() {
-  pictureApiService.fetchArticles().then(appendHitsMarkup);
+  pictureApiService.fetchArticles().then(appendHitsMarkup).then(scroll);
 }
 
 function appendHitsMarkup(hits) {
